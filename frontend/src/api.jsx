50e8a7e96ffa0543f2d1333dfe9b44db9a3381dc
Shubham Bendre <shubhambendre04@gmail.com>
2025-06-15 23:24:32 +0530

    Update api.jsx

--- frontend/src/api.jsx
+++ frontend/src/api.jsx
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://localhost:8080';
+const BASE_URL = 'https://complete-try.onrender.com';
 
 export const GetAllEmployees = async (search = '', page = 1, limit = 5, parentId) => {
     const url =
